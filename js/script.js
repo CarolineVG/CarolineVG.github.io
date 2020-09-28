f58a6dc42ec789539259316cee5d3b33472e3366
Caroline VG <CarolineVG@users.noreply.github.com>
2020-09-28 19:31:57 +0200

    fix css

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 var modal = ''
 
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 getTime()
 window.setInterval(function(){
   getTime()
